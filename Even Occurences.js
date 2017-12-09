2275b6a6a49370ebfc9e377f7ea8566e71341aa3
Aaron <aaronchantiles@gmail.com>
2017-12-08 17:13:08 -0800

    Even Occurences

--- Even Occurences.js
+++ Even Occurences.js
@@ -1,5 +1,5 @@
 /*
-Find the first item that occurs an even elementber of times in an array.
+Find the first item that occurs an even number of times in an array.
 Remember to handle multiple even-occurrence items and return the first one. 
 Return null if there are no even-occurrence items.
 */
